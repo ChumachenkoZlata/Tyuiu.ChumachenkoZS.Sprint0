f627585abededbbf9c529cfab77166d7795d326e
Zlata Chumachenko <zolotay2007.11.21@gmail.com>
2026-09-16 00:10:04 +0500

    Task5

---
+++ Tyuiu.ChumachenkoZS.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿namespace Tyuiu.ChumachenkoZS.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Переменная b = {0} на ноль делить нельзя", b);
+                return -1;
+            }
+            else
+            {
+                return (a / b);
+            }
+        }
+    }
+}
