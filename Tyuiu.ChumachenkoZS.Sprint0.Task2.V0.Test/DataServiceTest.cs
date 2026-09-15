78bf3197691352b0c1f262cfc6e73928221e5f84
Zlata Chumachenko <zolotay2007.11.21@gmail.com>
2026-09-15 20:43:20 +0500

    Task2

---
+++ Tyuiu.ChumachenkoZS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ChumachenkoZS.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ChumachenkoZS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Злата";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Злата", res);
+        }
+    }
+}
